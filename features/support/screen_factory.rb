093120ce91e99a20d18ae8110c8aa02bbb7a3605
Stephany Daneri <sdaneri@belatrixsf.com>
2018-02-19 03:38:23 -0500

    Removing commented code

--- features/support/screen_factory.rb
+++ features/support/screen_factory.rb
@@ -6,15 +6,5 @@ module ScreenFactory
     mobile_screen_instance
   end
 
-  # def on_base_mobile_screen(mobile_screen)
-  #
-  #   platform = Utilities.get_platform_under_test
-  #   platform = platform.to_s.capitalize
-  #   mobile_screen_name = "#{platform}#{mobile_screen.to_s}"
-  #
-  #   mobile_screen_name = "#{mobile_screen.to_s}"
-  #   on_mobile_screen(mobile_screen_name)
-  # end
-
 end
 World(ScreenFactory)
